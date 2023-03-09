5d136ab1ad7abbca40830050756f13d75c0d1448
your-name <katongole.roy100@gmail.com>
2023-03-09 13:51:58 +0300

    set state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -10,8 +10,8 @@ class Counter extends Component {
     }
 
     handleIncrement() {
-        console.log("Increment Clicked", this);
-    }
+        this.setState({ count: this.state.count + 1});
+    };
     render() {
 
         return (
