1f825adea4ec7531c731d32dae26cfd7bb3ea282
your-name <katongole.roy100@gmail.com>
2023-03-13 20:50:29 +0300

    passing Data to components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {
-        count: 0
+        value: this.props.value
     };
 
     constructor() {
@@ -11,7 +11,7 @@ class Counter extends Component {
 
     handleIncrement = product => {
         console.log(product);
-        this.setState({ count: this.state.count + 1});
+        this.setState({ value: this.state.value + 1});
     };
     render() {
 
